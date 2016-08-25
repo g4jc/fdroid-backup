72d28bf8c63825bbcd85281b9e12f9cdf7707599
Gaming4JC <g4jc@openmailbox.org>
2016-08-25 18:07:36 -0400

    Fix tabs for python3 support.

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # fdroid-backup
 A small script to create a local copy of f-droid repo. All APK, Icons, and signed jars. Sources are saved in their own folder. Integrity check on refresh. Public Domain.
 
+# PreRequisits
+* wget / sed / python
+
+* pip install pycurl
+
+
 # Usage
 1) Make sure you create a new working directory to put all the files. <-- important.
 
-2) Run fdroid_fresh.sh 
+2) Put all the scripts from this git into your new directory.
 
-3) Run fdroid_refresh.sh regularly.
+3) Run fdroid_fresh.sh 
 
-4) ??? profit!
+4) Run fdroid_refresh.sh regularly.
 
+5) ??? profit!
--- fdroid_backup_aio.py
+++ fdroid_backup_aio.py
@@ -1,5 +1,5 @@
 ## F-Droid Backup Script, to be used with bash scripts.
-## TODO: Grab package sources. Currently this script is capable of re-creating the entire binary repo to be used with F-Droid client... but it doesn't grab the source tarballs.
+## TODO: Find more sane way of detecting if pycurl finished
 import pycurl
 from xml.dom import minidom
 import re
@@ -17,7 +17,6 @@ with open('index.xml', 'wb') as f:
     c.setopt(c.WRITEDATA, f)
     c.perform()
     c.close()
-
 
 with open('index.jar', 'wb') as f:
     c = pycurl.Curl()
@@ -57,19 +56,19 @@ def getAPK(xml):
 
     hashvalues = []
     for thehash in hashes:
-        hashObj = hashes[counter].firstChild.data.encode('ascii', 'ignore')
-	counter += 1
-	hashvalues.append(hashObj)
-	list(set(hashObj))
+	    hashObj = hashes[counter].firstChild.data.encode('ascii', 'ignore')
+	    counter += 1
+	    hashvalues.append(hashObj)
+	    list(set(hashObj))
 
     counter = 0
 
     apknames = []
     for application in app:
-        apkObj = app[counter].firstChild.data.encode('ascii', 'ignore')
-	counter += 1
-        apknames.append(apkObj)
-	list(set(apknames))
+	    apkObj = app[counter].firstChild.data.encode('ascii', 'ignore')
+	    counter += 1
+	    apknames.append(apkObj)
+	    ist(set(apknames))
 
     result = [ item for tup in zip(hashvalues,apknames) for item in tup ]
     
@@ -86,15 +85,15 @@ def getAPK(xml):
     f.close()
     
     with open("download_apk.txt", "a") as f:
-      f.write("\n".join(append_asc_extension)) ## Append .asc signatures to same file.
-      f.close()
+	    f.write("\n".join(append_asc_extension)) ## Append .asc signatures to same file.
+	    f.close()
 
-    f = open( 'apk.txt', 'w' )
-    f.write("\n".join(apknames)) ## Print only apk names (Required for grepping during re-dump/refresh script)
-    f.close()
-    f = open( 'apk.txt', 'a' )
-    f.write('\n' + "\n".join([re.sub(regex_line_end, '.asc', string) for string in apknames])) ## Append .asc signatures to same file.
-    f.close()
+	    f = open( 'apk.txt', 'w' )
+	    f.write("\n".join(apknames)) ## Print only apk names (Required for grepping during re-dump/refresh script)
+	    f.close()
+	    f = open( 'apk.txt', 'a' )
+	    f.write('\n' + "\n".join([re.sub(regex_line_end, '.asc', string) for string in apknames])) ## Append .asc signatures to same file.
+	    f.close()
 
 
 
@@ -110,56 +109,50 @@ def getIcons(xml):
 
     iconsnames = []
     for iconlocation in icon:
-        iconsObj = icon[counter].firstChild.data.encode('ascii', 'ignore')
-	counter += 1
-        iconsnames.append(iconsObj)
-	list(set(iconsnames))
+	    iconsObj = icon[counter].firstChild.data.encode('ascii', 'ignore')
+	    counter += 1
+	    iconsnames.append(iconsObj)
+	    list(set(iconsnames))
     ## Magical Regex to create downloadable url's
-    add_fdroid_url = [re.sub(regex_line_start, (repo_url + 'icons/'), string) for string in iconsnames]
+	    add_fdroid_url = [re.sub(regex_line_start, (repo_url + 'icons/'), string) for string in iconsnames]
     ##
-    f = open( 'download_icons.txt', 'w' ) ## TIP: change this line to 'with open("download_apk.txt", "a") as f:' if you want all url's in single file.
-    f.write("\n".join(add_fdroid_url)) ## Convert list to line breaks, prepend the url, and print to file.
-    f.close()
+	    f = open( 'download_icons.txt', 'w' ) ## TIP: change this line to 'with open("download_apk.txt", "a") as f:' if you want all url's in single file.
+	    f.write("\n".join(add_fdroid_url)) ## Convert list to line breaks, prepend the url, and print to file.
+	    f.close()
 
-    f = open( 'icons.txt', 'w' )
-    f.write("\n".join(iconsnames)) ## Print only icon names (Required for grepping during re-dump/refresh script)
-    f.close()
+	    f = open( 'icons.txt', 'w' )
+	    f.write("\n".join(iconsnames)) ## Print only icon names (Required for grepping during re-dump/refresh script)
+	    f.close()
 
 
 def getSource(xml):
-    """
-    Print out all source tarballs found in xml
-    """
-    doc = minidom.parse(xml)
-    node = doc.documentElement
-    fdroidNode = doc.firstChild
-    counter = 0
-    source = fdroidNode.getElementsByTagName("srcname")
-
-    sourcenames = []
-    for sourcelocation in source:
-        sourceObj = source[counter].firstChild.data.encode('ascii', 'ignore')
-	counter += 1
-        sourcenames.append(sourceObj)
-	list(set(sourcenames))
+	"""
+	Print out all source tarballs found in xml
+	"""
+	doc = minidom.parse(xml)
+	node = doc.documentElement
+	fdroidNode = doc.firstChild
+	counter = 0
+	source = fdroidNode.getElementsByTagName("srcname")
+	
+	sourcenames = []
+	for sourcelocation in source:
+		sourceObj = source[counter].firstChild.data.encode('ascii', 'ignore')
+		counter += 1
+		sourcenames.append(sourceObj)
+		list(set(sourcenames))
     ## Magical Regex to create downloadable url's
-    add_fdroid_url = [re.sub(regex_line_start, repo_url, string) for string in sourcenames]
-    ##
-    f = open( 'download_sources.txt', 'w' )
-    f.write("\n".join(add_fdroid_url)) ## Convert list to line breaks, prepend the url, and print to file.
-    f.close()
-
-    f = open( 'sources.txt', 'w' )
-    f.write("\n".join(sourcenames)) ## Print only source tarball names (Required for grepping during re-dump/refresh script)
-    f.close()
+		ladd_fdroid_url = [re.sub(regex_line_start, repo_url, string) for string in sourcenames]
+		f = open( 'download_sources.txt', 'w' )
+		f.write("\n".join(add_fdroid_url)) ## Convert list to line breaks, prepend the url, and print to file.
+		f.close()
+		f = open( 'sources.txt', 'w' )
+		f.write("\n".join(sourcenames)) ## Print only source tarball names (Required for grepping during re-dump/refresh script)
+		f.close()
 
 
 if __name__ == "__main__":
-    document = 'index.xml'
-    getAPK(document)
-    getIcons(document)
-    getSource(document)
-
-
-
-
+	document = 'index.xml'
+	getAPK(document)
+	getIcons(document)
+	getSource(document)
